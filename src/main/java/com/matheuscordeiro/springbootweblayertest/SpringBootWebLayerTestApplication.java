10a42925100b2f65ebd80e634926348e4f51ea30
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-11-11 20:54:23 -0300

    First commit

---
+++ src/main/java/com/matheuscordeiro/springbootweblayertest/SpringBootWebLayerTestApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.springbootweblayertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebLayerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebLayerTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscordeiro/springbootweblayertest/SpringBootWebLayerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.springbootweblayertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebLayerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
